8c7e49e375980480cc6c5ed308b87f4574ef4718
hirohiro-sys <katohiro8843@icloud.com>
2023-09-28 19:08:57 +0900

    コメントアウトの追加

--- snippets/urls.py
+++ snippets/urls.py
@@ -12,3 +12,18 @@ urlpatterns = [
     path("<int:snippet_id>/edit/", SnippetEditView.as_view(), name="snippet_edit"),
     path("<int:snippet_id>/comments/", CommentNewView.as_view(), name="comment_new"),
 ]
+
+
+'''関数ベースビュー
+from django.urls import path
+from snippets import views
+
+
+urlpatterns = [
+    path("new/", views.snippet_new, name="snippet_new"),
+    path("<int:snippet_id>/", views.snippet_detail, name="snippet_detail"),
+    path("<int:snippet_id>/edit/", views.snippet_edit, name="snippet_edit"),
+    path("<int:snippet_id>/comments/", views.comment_new, name="comment_new"),
+
+]
+'''
