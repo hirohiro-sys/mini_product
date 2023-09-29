dae769d416a72d6524899b5457f9e4836ae19db3
hirohiro-sys <katohiro8843@icloud.com>
2023-09-29 13:54:58 +0900

    デザイン微調整

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from django.views.generic import CreateView
 
+
 urlpatterns = [
     path('signup/', CreateView.as_view(
         template_name = 'accounts/signup.html',
@@ -15,4 +16,5 @@ urlpatterns = [
 
     ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    
 ]
