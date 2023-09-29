e83a6afcf2b0f1c7210bff77d320c1aa0addf6ea
hirohiro-sys <katohiro8843@icloud.com>
2023-09-29 18:40:15 +0900

    クラスベースビューのコード修正

--- snippets/views.py
+++ snippets/views.py
@@ -6,13 +6,16 @@ from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
-from django.views.generic import View, ListView
+from django.views.generic import View, ListView,CreateView, UpdateView
+from django.urls import reverse_lazy
 from django.core.exceptions import PermissionDenied
 
 
 from snippets.forms import SnippetForm, CommentForm
 from snippets.models import Snippet, Comment
 
+
+# ページネーション、検索機能、更新日でソート機能実装
 class TopView(ListView):
     model = Snippet
     template_name = "snippets/top.html"
@@ -26,39 +29,25 @@ class TopView(ListView):
         if q := query.get('q'):
             queryset = queryset.filter(Q(code__icontains=q)|Q(title__icontains=q))
 
-        return queryset.order_by('-created_at')
-
-
-    # def get(self, request):
-    #     snippets = Snippet.objects.all()
-    #     context = {"snippets": snippets}
-    #     return render(request, "snippets/top.html", context)
-    
-    # def get_queryset(self):
-    #     snippets = super().get_queryset()
-    #     return snippets.order_by('-updated_at')
+        return queryset.order_by('-updated_at')
     
 
-class SnippetNewView(View):
-    @method_decorator(login_required)
-    def get(self, request):
-        form = SnippetForm()
-        return render(request, "snippets/snippet_new.html", {'form': form})
+@method_decorator(login_required, name='dispatch')
+class SnippetNewView(CreateView):
+    model = Snippet
+    form_class = SnippetForm
+    template_name = 'snippets/snippet_new.html'
+    success_url = reverse_lazy('snippet_detail')  # Redirect to detail view after successful creation
 
-    @method_decorator(login_required)
-    def post(self, request):
-        form = SnippetForm(request.POST)
-        if form.is_valid():
-            snippet = form.save(commit=False)
-            snippet.created_by = request.user
-            snippet.save()
-            messages.add_message(request, messages.SUCCESS,
-                                 "スニペットを作成しました。")
-            return redirect('snippet_detail', snippet_id=snippet.pk)
-        else:
-            messages.add_message(request, messages.ERROR,
-                                 "スニペットの作成に失敗しました。")
-            return render(request, "snippets/snippet_new.html", {'form': form})
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        messages.success(self.request, "スニペットを作成しました。")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "スニペットの作成に失敗しました。")
+        return super().form_invalid(form)
+        
 
 class SnippetEditView(View):
     @method_decorator(login_required)
@@ -86,6 +75,7 @@ class SnippetEditView(View):
             messages.add_message(request, messages.ERROR,
                                  "スニペットの更新に失敗しました。")
             return render(request, 'snippets/snippet_edit.html', {'form': form})
+        
 
 class SnippetDetailView(View):
     @method_decorator(login_required)
@@ -98,6 +88,7 @@ class SnippetDetailView(View):
             'comments': comments,
             'comment_form': comment_form,
         })
+    
 
 class CommentNewView(View):
     @method_decorator(login_required)
@@ -115,96 +106,3 @@ class CommentNewView(View):
             messages.add_message(request, messages.ERROR,
                                  "コメントの投稿に失敗しました。")
         return redirect('snippet_detail', snippet_id=snippet_id)
-    
-
-
-'''関数ベースビュー
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
-
-
-
-from snippets.forms import SnippetForm, CommentForm
-from snippets.models import Snippet, Comment
-
-
-
-
-def top(request):
-    snippets = Snippet.objects.all()
-    context = {"snippets": snippets}
-    return render(request, "snippets/top.html", context)
-
-
-@login_required
-def snippet_new(request):
-    if request.method == 'POST':
-        form = SnippetForm(request.POST)
-        if form.is_valid():
-            snippet = form.save(commit=False)
-            snippet.created_by = request.user
-            snippet.save()
-            messages.add_message(request, messages.SUCCESS,
-                                 "スニペットを作成しました。")
-            return redirect(snippet_detail, snippet_id=snippet.pk)
-        else:
-            messages.add_message(request, messages.ERROR,
-                                 "スニペットの作成に失敗しました。")
-    else:
-        form = SnippetForm()
-    return render(request, "snippets/snippet_new.html", {'form': form})
-
-
-@login_required
-def snippet_edit(request, snippet_id):
-    snippet = get_object_or_404(Snippet, pk=snippet_id)
-    if snippet.created_by_id != request.user.id:
-        return HttpResponseForbidden("このスニペットの編集は許可されていません。")
-
-    if request.method == "POST":
-        form = SnippetForm(request.POST, instance=snippet)
-        if form.is_valid():
-            form.save()
-            messages.add_message(request, messages.SUCCESS,
-                                 "スニペットを更新しました。")
-            return redirect('snippet_detail', snippet_id=snippet_id)
-        else:
-            messages.add_message(request, messages.ERROR,
-                                 "スニペットの更新に失敗しました。")
-    else:
-        form = SnippetForm(instance=snippet)
-    return render(request, 'snippets/snippet_edit.html', {'form': form})
-
-
-@login_required
-def snippet_detail(request, snippet_id):
-    snippet = get_object_or_404(Snippet, pk=snippet_id)
-    comments = Comment.objects.filter(commented_to=snippet_id).all()
-    comment_form = CommentForm()
-
-    return render(request, "snippets/snippet_detail.html", {
-        'snippet': snippet,
-        'comments': comments,
-        'comment_form': comment_form,
-    })
-
-
-@login_required
-def comment_new(request, snippet_id):
-    snippet = get_object_or_404(Snippet, pk=snippet_id)
-
-    form = CommentForm(request.POST)
-    if form.is_valid():
-        comment = form.save(commit=False)
-        comment.commented_to = snippet
-        comment.commented_by = request.user
-        comment.save()
-        messages.add_message(request, messages.SUCCESS,
-                             "コメントを投稿しました。")
-    else:
-        messages.add_message(request, messages.ERROR,
-                             "コメントの投稿に失敗しました。")
-    return redirect('snippet_detail', snippet_id=snippet_id)
-'''
